d4bb0a8465eaf7cdd25fd56b5850d2bac36f2d99
Vlad <vlads1976@gmail.com>
2022-07-31 16:56:49 +0200

    new solved tasks.

--- codewar_2.py
+++ codewar_2.py
@@ -63,12 +63,32 @@ def frame(balls):
 
 
 balls = "R15Bk16YGBnBeP"
-print(frame(balls))
+(frame(balls))
 
 
 def alphanumeric(password):
     """Not very secure"""
-    res = re.match('^[\da-zA-Z]+$', password)
+    res = re.match(r'^[\da-zA-Z]+$', password)
     if res:
         return True
     return False
+
+
+def well(arr):
+    """7 kyu Well of Ideas - Harder Version"""
+
+    count = 0
+    for i in arr:
+        for n in i:
+            if str(n).lower() == 'good':
+                count += 1
+    if count == 1 or count == 2:
+        return 'Publish!'
+    elif count > 2:
+        return 'I smell a series!'
+    return 'Fail!'
+
+
+assert well([['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad']]) == 'Fail!'
+assert well([['gOOd', 'bad', 'BAD', 'bad', 'bad'], ['bad', 'bAd', 'bad'], ['GOOD', 'bad', 'bad', 'bAd']]) == 'Publish!'
+assert well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]) == 'I smell a series!'
